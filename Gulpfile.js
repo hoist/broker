528eae576c585e359d3bf3cbcffa32c244a816ff
Owen Evans <owen@bgeek.net>
2015-06-25 13:33:58 +1200

    now it should fail correctly

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,17 +2,19 @@
 var gulp = require('gulp');
 var requireDir = require('require-dir');
 require('git-guppy')(gulp);
+var helpers = require('./gulp/helpers');
 
 requireDir('./gulp/tasks', {
   recurse: true
 });
-gulp.task('test', ['eslint-build', 'mocha-server']);
+gulp.task('test', ['eslint-build', 'mocha-server'], function (cb) {
+
+
+  cb(helpers.getError());
+});
 gulp.task('default', function () {
   return gulp.start('eslint-build',
-    'mocha-server',
-    function (cb) {
-      cb(global.error);
-    });
+    'mocha-server');
 });
 
 gulp.task('post-commit', ['test', 'esdoc']);
--- gulp/helpers.js
+++ gulp/helpers.js
@@ -1,14 +1,17 @@
 'use strict';
 var notifier = require('node-notifier');
 
-
+var _error;
 module.exports = {
+  getError: function () {
+    return _error;
+  },
   errorHandler: function (err) {
     notifier.notify({
       title: 'A Gulp error occurred',
       message: err.message
     });
-    global.error = err;
+    _error = err;
     console.log('Error:', err.message, err.stack);
   }
 };
